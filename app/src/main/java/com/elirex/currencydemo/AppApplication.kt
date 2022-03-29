564b07022974fbff069dafa583128bd13ca5f737
Wang, Sheng-Yuan <sheng79225@gmail.com>
2022-03-29 12:41:16 +0800

    Create AppApplication

---
+++ app/src/main/java/com/elirex/currencydemo/AppApplication.kt
@@ -0,0 +1,18 @@
+package com.elirex.currencydemo
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
+
+@HiltAndroidApp
+class AppApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        if (BuildConfig.DEBUG)  {
+            Timber.plant(Timber.DebugTree())
+        }
+    }
+
+}
